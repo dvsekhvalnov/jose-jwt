2f4e7c56f683442cbb15b4fd28f88aa4d6d11abf
DV <dvsekhvalnov@pingidentity.com>
2016-02-09 23:56:02 +0300

    resurecting ES-* for netcore

--- jose-jwt/jws/EcdsaUsingSha.cs
+++ jose-jwt/jws/EcdsaUsingSha.cs
@@ -14,7 +14,6 @@ namespace Jose
 
         public byte[] Sign(byte[] securedInput, object key)
         {
-        #if DNX451  
             var privateKey = Ensure.Type<CngKey>(key, "EcdsaUsingSha alg expects key to be of CngKey type.");
 
             Ensure.BitSize(privateKey.KeySize, keySize, string.Format("ECDSA algorithm expected key of size {0} bits, but was given {1} bits", keySize, privateKey.KeySize));
@@ -23,24 +22,23 @@ namespace Jose
             {
                 using (var signer = new ECDsaCng(privateKey))
                 {
+                #if DNX451
                     signer.HashAlgorithm = Hash;
 
                     return signer.SignData(securedInput);
+                #elif DNXCORE50
+                    return signer.SignData(securedInput, Hash);
+                #endif
                 }
             }
             catch (CryptographicException e)
             {
                 throw new JoseException("Unable to sign content.", e);    
             }
-
-        #elif DNXCORE50
-            throw new NotImplementedException("not yet");
-        #endif
         }
 
         public bool Verify(byte[] signature, byte[] securedInput, object key)
         {
-        #if DNX451
             var publicKey = Ensure.Type<CngKey>(key, "EcdsaUsingSha alg expects key to be of CngKey type.");
 
             Ensure.BitSize(publicKey.KeySize, keySize, string.Format("ECDSA algorithm expected key of size {0} bits, but was given {1} bits", keySize, publicKey.KeySize));
@@ -49,20 +47,22 @@ namespace Jose
             {
                 using (var signer = new ECDsaCng(publicKey))
                 {
+                #if DNX451
                     signer.HashAlgorithm = Hash;
                 
                     return signer.VerifyData(securedInput, signature);
+                #elif DNXCORE50
+                    return signer.VerifyData(securedInput, signature, Hash);
+                #endif
                 }
             }
             catch (CryptographicException e)
             {
                 return false;
             }
-        #elif DNXCORE50
-            throw new NotImplementedException("not yet");
-        #endif
         }
 
+#if DNX451
         protected CngAlgorithm Hash
         {
             get
@@ -77,5 +77,22 @@ namespace Jose
                 throw new ArgumentException(string.Format("Unsupported key size: '{0} bytes'", keySize));
             }
         }
+
+#elif DNXCORE50
+        protected HashAlgorithmName Hash
+        {
+            get
+            {
+                if (keySize == 256)
+                    return HashAlgorithmName.SHA256;
+                if (keySize == 384)
+                    return HashAlgorithmName.SHA384;
+                if (keySize == 521)
+                    return HashAlgorithmName.SHA512;
+
+                throw new ArgumentException(string.Format("Unsupported key size: '{0} bytes'", keySize));
+            }
+        }
+#endif
     }
 }
