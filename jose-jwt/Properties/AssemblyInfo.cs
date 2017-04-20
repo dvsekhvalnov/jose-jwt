27beaff995bdd1385d58846062999e39d5b773a9
DV <dvsekhvalnov@pingidentity.com>
2017-04-20 17:56:39 +0300

    bump up version to 2.3

--- jose-jwt/Properties/AssemblyInfo.cs
+++ jose-jwt/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("dvsekhvalnov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
