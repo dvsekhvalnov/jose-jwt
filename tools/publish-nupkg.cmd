2416878ab89114c318614cf202c1da09f986c0e2
DV <dvsekhvalnov@pingidentity.com>
2017-06-21 19:55:39 +0300

    bump up version to 2.4, prepare .nuget

--- tools/publish-nupkg.cmd
+++ tools/publish-nupkg.cmd
@@ -1,2 +1,2 @@
 cd ..\jose-jwt\bin\Release
-..\..\..\tools\nuget.exe Push jose-jwt.2.3.0.nupkg -Source https://www.nuget.org
+..\..\..\tools\nuget.exe Push jose-jwt.2.4.0.nupkg -Source https://www.nuget.org
