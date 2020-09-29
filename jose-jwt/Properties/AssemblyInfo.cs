16ecbd7cf8f54ee38c6592e82ffc64a6efa72198
DV <dvsekhvalnov@gmail.com>
2020-09-29 10:05:16 -0700

    more cleanup

--- jose-jwt/Properties/AssemblyInfo.cs
+++ jose-jwt/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("dvsekhvalnov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.5.1.0")]
-[assembly: AssemblyFileVersion("2.5.1.0")]
+[assembly: AssemblyVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.5.0")]
