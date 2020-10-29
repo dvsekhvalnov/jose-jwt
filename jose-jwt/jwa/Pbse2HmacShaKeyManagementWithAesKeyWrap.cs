3b8fa9b79a938a5a655a16397b055e4b42f47c7d
Vadim Doroginin <archimed7592@gmail.com>
2020-10-29 13:53:42 +0300

    Fix hardcoded iterationsCount (issue #124) - simplifying things

--- jose-jwt/jwa/Pbse2HmacShaKeyManagementWithAesKeyWrap.cs
+++ jose-jwt/jwa/Pbse2HmacShaKeyManagementWithAesKeyWrap.cs
@@ -24,18 +24,15 @@ namespace Jose
             byte[] sharedKey = Encoding.UTF8.GetBytes(sharedPassphrase);
             byte[] algId = Encoding.UTF8.GetBytes((string)header["alg"]);
 
-            int iterationCount;
+            int iterationCount = 8192;
             if (header.TryGetValue("p2c", out var iterationCountObj))
             {
                 iterationCount = Ensure.Type<int>(iterationCountObj, "Pbse2HmacShaKeyManagementWithAesKeyWrap management algorithm expectes p2c to be int.");
             }
-            else
-            {
-                iterationCount = 8192;
-                header["p2c"] = iterationCount;
-            }
+
             byte[] saltInput = Arrays.Random(96); //12 bytes
 
+            header["p2c"] = iterationCount;
             header["p2s"] = Base64Url.Encode(saltInput);
 
             byte[] salt = Arrays.Concat(algId, Arrays.Zero, saltInput);
