d3bd4281a87103ad459560a80da905416b01a200
Christiaan Rakowski <cs.rakowski@gmail.com>
2022-04-10 16:59:30 +0200

    Fixed JwkTest

--- UnitTests/jwk/JwkSetTest.cs
+++ UnitTests/jwk/JwkSetTest.cs
@@ -8,7 +8,7 @@ namespace UnitTests
 {
     public class JwkSetTest
     {
-        private TestConsole Console;
+        private readonly TestConsole Console;
 
         public JwkSetTest(ITestOutputHelper output)
         {
