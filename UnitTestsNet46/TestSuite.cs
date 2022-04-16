1b4c0f71613f51a45b163cced4ef40d5e6ad1813
Christiaan Rakowski <cs.rakowski@gmail.com>
2022-04-16 21:05:30 +0200

    Cleaned up Test Utils Region

--- UnitTestsNet46/TestSuite.cs
+++ UnitTestsNet46/TestSuite.cs
@@ -3535,9 +3535,9 @@ namespace UnitTests
             Assert.Equal(test.sub, "testperson@example.com");
         }
 
-        #region test utils
+        #region Test Utils
 
-        private RSACryptoServiceProvider PrivKey()
+        private static RSACryptoServiceProvider PrivKey()
         {
             var key = (RSACryptoServiceProvider)X509().PrivateKey;
 
@@ -3547,41 +3547,41 @@ namespace UnitTests
             return newKey;
         }
 
-        private RSA PrivRsaKey()
+        private static RSA PrivRsaKey()
         {
             return X509().GetRSAPrivateKey();
         }
 
-        private RSACryptoServiceProvider PubKey()
+        private static RSACryptoServiceProvider PubKey()
         {
             return (RSACryptoServiceProvider)X509().PublicKey.Key;
         }
 
-        private RSA PubRsaKey()
+        private static RSA PubRsaKey()
         {
             return X509().GetRSAPublicKey();
         }
 
-        private X509Certificate2 X509()
+        private static X509Certificate2 X509()
         {
             return new X509Certificate2("jwt-2048.p12", "1", X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet);
         }
 
-        private ECDsa ECDSa256Public()
+        private static ECDsa ECDSa256Public()
         {
             var x095 = new X509Certificate2("ecc256.p12", "12345");
 
             return x095.GetECDsaPublicKey();
         }
 
-        private ECDsa ECDSa256Private()
+        private static ECDsa ECDSa256Private()
         {
             var x095 = new X509Certificate2("ecc256.p12", "12345");
 
             return x095.GetECDsaPrivateKey();
         }
 
-        private CngKey Ecc256Public(CngKeyUsages usage = CngKeyUsages.Signing)
+        private static CngKey Ecc256Public(CngKeyUsages usage = CngKeyUsages.Signing)
         {
             byte[] x = { 4, 114, 29, 223, 58, 3, 191, 170, 67, 128, 229, 33, 242, 178, 157, 150, 133, 25, 209, 139, 166, 69, 55, 26, 84, 48, 169, 165, 67, 232, 98, 9 };
             byte[] y = { 131, 116, 8, 14, 22, 150, 18, 75, 24, 181, 159, 78, 90, 51, 71, 159, 214, 186, 250, 47, 207, 246, 142, 127, 54, 183, 72, 72, 253, 21, 88, 53 };
@@ -3590,7 +3590,7 @@ namespace UnitTests
             return EccKey.New(x, y, usage: usage);
         }
 
-        private CngKey Ecc384Public()
+        private static CngKey Ecc384Public()
         {
             byte[] x = { 70, 151, 220, 179, 62, 0, 79, 232, 114, 64, 58, 75, 91, 209, 232, 128, 7, 137, 151, 42, 13, 148, 15, 133, 93, 215, 7, 3, 136, 124, 14, 101, 242, 207, 192, 69, 212, 145, 88, 59, 222, 33, 127, 46, 30, 218, 175, 79 };
             byte[] y = { 189, 202, 196, 30, 153, 53, 22, 122, 171, 4, 188, 42, 71, 2, 9, 193, 191, 17, 111, 180, 78, 6, 110, 153, 240, 147, 203, 45, 152, 236, 181, 156, 232, 223, 227, 148, 68, 148, 221, 176, 57, 149, 44, 203, 83, 85, 75, 55 };
@@ -3598,7 +3598,7 @@ namespace UnitTests
             return EccKey.New(x, y);
         }
 
-        private CngKey Ecc384Private()
+        private static CngKey Ecc384Private()
         {
             byte[] x = { 70, 151, 220, 179, 62, 0, 79, 232, 114, 64, 58, 75, 91, 209, 232, 128, 7, 137, 151, 42, 13, 148, 15, 133, 93, 215, 7, 3, 136, 124, 14, 101, 242, 207, 192, 69, 212, 145, 88, 59, 222, 33, 127, 46, 30, 218, 175, 79 };
             byte[] y = { 189, 202, 196, 30, 153, 53, 22, 122, 171, 4, 188, 42, 71, 2, 9, 193, 191, 17, 111, 180, 78, 6, 110, 153, 240, 147, 203, 45, 152, 236, 181, 156, 232, 223, 227, 148, 68, 148, 221, 176, 57, 149, 44, 203, 83, 85, 75, 55 };
@@ -3607,7 +3607,7 @@ namespace UnitTests
             return EccKey.New(x, y, d);
         }
 
-        private CngKey Ecc256Private(CngKeyUsages usage = CngKeyUsages.Signing)
+        private static CngKey Ecc256Private(CngKeyUsages usage = CngKeyUsages.Signing)
         {
             byte[] x = { 4, 114, 29, 223, 58, 3, 191, 170, 67, 128, 229, 33, 242, 178, 157, 150, 133, 25, 209, 139, 166, 69, 55, 26, 84, 48, 169, 165, 67, 232, 98, 9 };
             byte[] y = { 131, 116, 8, 14, 22, 150, 18, 75, 24, 181, 159, 78, 90, 51, 71, 159, 214, 186, 250, 47, 207, 246, 142, 127, 54, 183, 72, 72, 253, 21, 88, 53 };
@@ -3616,7 +3616,7 @@ namespace UnitTests
             return EccKey.New(x, y, d, usage);
         }
 
-        private CngKey Ecc512Public()
+        private static CngKey Ecc512Public()
         {
             byte[] x = { 0, 248, 73, 203, 53, 184, 34, 69, 111, 217, 230, 255, 108, 212, 241, 229, 95, 239, 93, 131, 100, 37, 86, 152, 87, 98, 170, 43, 25, 35, 80, 137, 62, 112, 197, 113, 138, 116, 114, 55, 165, 128, 8, 139, 148, 237, 109, 121, 40, 205, 3, 61, 127, 28, 195, 58, 43, 228, 224, 228, 82, 224, 219, 148, 204, 96 };
             byte[] y = { 0, 60, 71, 97, 112, 106, 35, 121, 80, 182, 20, 167, 143, 8, 246, 108, 234, 160, 193, 10, 3, 148, 45, 11, 58, 177, 190, 172, 26, 178, 188, 240, 91, 25, 67, 79, 64, 241, 203, 65, 223, 218, 12, 227, 82, 178, 66, 160, 19, 194, 217, 172, 61, 250, 23, 78, 218, 130, 160, 105, 216, 208, 235, 124, 46, 32 };
@@ -3624,7 +3624,7 @@ namespace UnitTests
             return EccKey.New(x, y);
         }
 
-        private CngKey Ecc512Private()
+        private static CngKey Ecc512Private()
         {
             byte[] x = { 0, 248, 73, 203, 53, 184, 34, 69, 111, 217, 230, 255, 108, 212, 241, 229, 95, 239, 93, 131, 100, 37, 86, 152, 87, 98, 170, 43, 25, 35, 80, 137, 62, 112, 197, 113, 138, 116, 114, 55, 165, 128, 8, 139, 148, 237, 109, 121, 40, 205, 3, 61, 127, 28, 195, 58, 43, 228, 224, 228, 82, 224, 219, 148, 204, 96 };
             byte[] y = { 0, 60, 71, 97, 112, 106, 35, 121, 80, 182, 20, 167, 143, 8, 246, 108, 234, 160, 193, 10, 3, 148, 45, 11, 58, 177, 190, 172, 26, 178, 188, 240, 91, 25, 67, 79, 64, 241, 203, 65, 223, 218, 12, 227, 82, 178, 66, 160, 19, 194, 217, 172, 61, 250, 23, 78, 218, 130, 160, 105, 216, 208, 235, 124, 46, 32 };
@@ -3633,7 +3633,7 @@ namespace UnitTests
             return EccKey.New(x, y, d);
         }
 
-        #endregion
+        #endregion Test Utils
     }
 
     public class TestPayloadModel
