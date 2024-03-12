dc54f51aaf056fa3e48080881da95f8b114c38a0
DV <dvsekhvalnov@gmail.com>
2024-03-12 10:10:48 +0300

    ECDSa ECDH KW AES-GCM unit tests

--- UnitTestsNet46/TestSuite.cs
+++ UnitTestsNet46/TestSuite.cs
@@ -3468,6 +3468,30 @@ namespace UnitTests
         }
 
         [Fact]
+        public void Encrypt_ECDH_ES_A128KW_A128GCM_ECDSaKey()
+        {
+            //given
+            const string json = @"{""exp"":1389189552,""sub"":""alice"",""nbf"":1389188952,""aud"":[""https:\/\/app-one.com"",""https:\/\/app-two.com""],""iss"":""https:\/\/openid.net"",""jti"":""e543edf6-edf0-4348-8940-c4e28614d463"",""iat"":1389188952}";
+
+            //when
+            string token = Jose.JWT.Encode(json, Ecdh256Public(), JweAlgorithm.ECDH_ES_A128KW, JweEncryption.A128GCM);
+
+            //then
+            Console.Out.WriteLine("ECDH-ES+A128KW A128GCM ECDSaKey = {0}", token);
+
+            string[] parts = token.Split('.');
+
+            Assert.Equal(5, parts.Length); //Make sure 5 parts
+            Assert.Equal(231, parts[0].Length); //Header size
+            Assert.Equal(32, parts[1].Length); //CEK size
+            Assert.Equal(16, parts[2].Length); //IV size
+            Assert.Equal(262, parts[3].Length); //cipher text size
+            Assert.Equal(22, parts[4].Length); //auth tag size
+
+            Assert.Equal(json, Jose.JWT.Decode(token, Ecdh256Private(CngKeyUsages.KeyAgreement)));
+        }
+
+        [Fact]
         public void Encrypt_ECDH_ES_A128KW_A128GCM_JsonWebKey()
         {
             //given
@@ -3522,6 +3546,30 @@ namespace UnitTests
         }
 
         [Fact]
+        public void Encrypt_ECDH_ES_A192KW_A192GCM_ECDSaKey()
+        {
+            //given
+            const string json = @"{""exp"":1389189552,""sub"":""alice"",""nbf"":1389188952,""aud"":[""https:\/\/app-one.com"",""https:\/\/app-two.com""],""iss"":""https:\/\/openid.net"",""jti"":""e543edf6-edf0-4348-8940-c4e28614d463"",""iat"":1389188952}";
+
+            //when
+            string token = Jose.JWT.Encode(json, ECDSa384Public(), JweAlgorithm.ECDH_ES_A192KW, JweEncryption.A192GCM);
+
+            //then
+            Console.Out.WriteLine("ECDH-ES+A192KW A192GCM ECDSaKey = {0}", token);
+
+            string[] parts = token.Split('.');
+
+            Assert.Equal(5, parts.Length); //Make sure 5 parts
+            Assert.Equal(287, parts[0].Length); //Header size
+            Assert.Equal(43, parts[1].Length); //CEK size
+            Assert.Equal(16, parts[2].Length); //IV size
+            Assert.Equal(262, parts[3].Length); //cipher text size
+            Assert.Equal(22, parts[4].Length); //auth tag size
+
+            Assert.Equal(json, Jose.JWT.Decode(token, Ecdh384Private(CngKeyUsages.KeyAgreement)));
+        }
+
+        [Fact]
         public void Encrypt_ECDH_ES_A192KW_A192GCM_JsonWebKey()
         {
             //given
@@ -3561,6 +3609,30 @@ namespace UnitTests
 
             //then
             Console.Out.WriteLine("ECDH-ES+A256KW A256GCM EcdhKey = {0}", token);
+
+            string[] parts = token.Split('.');
+
+            Assert.Equal(5, parts.Length); //Make sure 5 parts
+            Assert.Equal(351, parts[0].Length); //Header size
+            Assert.Equal(54, parts[1].Length); //CEK size
+            Assert.Equal(16, parts[2].Length); //IV size
+            Assert.Equal(262, parts[3].Length); //cipher text size
+            Assert.Equal(22, parts[4].Length); //auth tag size
+
+            Assert.Equal(json, Jose.JWT.Decode(token, Ecdh512Private(CngKeyUsages.KeyAgreement)));
+        }
+
+        [Fact]
+        public void Encrypt_ECDH_ES_A256KW_A256GCM_ECDSaKey()
+        {
+            //given
+            const string json = @"{""exp"":1389189552,""sub"":""alice"",""nbf"":1389188952,""aud"":[""https:\/\/app-one.com"",""https:\/\/app-two.com""],""iss"":""https:\/\/openid.net"",""jti"":""e543edf6-edf0-4348-8940-c4e28614d463"",""iat"":1389188952}";
+
+            //when
+            string token = Jose.JWT.Encode(json, ECDSa521Public(), JweAlgorithm.ECDH_ES_A256KW, JweEncryption.A256GCM);
+
+            //then
+            Console.Out.WriteLine("ECDH-ES+A256KW A256GCM ECDSaKey = {0}", token);
 
             string[] parts = token.Split('.');
 
